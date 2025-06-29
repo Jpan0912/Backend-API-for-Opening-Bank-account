cf6d3eb70f6b7221794c1d8a5346d768f2f2b1d2
Jpan0912 <74218391+Jpan0912@users.noreply.github.com>
2025-06-29 13:46:12 +1200

    setting up springboot api

--- untitled/build.gradle.kts
+++ untitled/build.gradle.kts
@@ -1,4 +1,7 @@
 plugins {
+    id("org.springframework.boot") version "3.2.5"
+    id("io.spring.dependency-management") version "1.1.4"
+    application
     id("java")
 }
 
@@ -12,6 +15,8 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 java {
@@ -19,6 +24,11 @@ java {
         languageVersion.set(JavaLanguageVersion.of(17))
     }
 }
+
+application {
+    mainClass.set("com.john.bank.Application")
+}
+
 
 tasks.test {
     useJUnitPlatform()
